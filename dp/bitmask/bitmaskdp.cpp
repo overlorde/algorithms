76ecee30c8e3f74ab548d7ded2ba722f094afcfe
saif <farhansaif488@gmail.com>
2020-09-09 15:35:54 +0600

    --- Auto-Git Commit ---

--- dp/bitmask/bitmaskdp.cpp
+++ dp/bitmask/bitmaskdp.cpp
@@ -11,21 +11,40 @@ int n;
 
 bool isTurnOn(int num,int pos)
 {
+    return num & (1<<pos);
+}
+int turnOn(int num,int pos)
+{
     return num | (1<<pos);
 }
-bool turnOn()
-{
 
-}
-
-int f()
+int f(int i,int mask)
 {
-    
+    if(mask == (1<<n)-1)
+    {
+        return w[i][0];
+    }
+    if(mem[i][mask] != -1)
+    {
+        return mem[i][mask];
+    }
+    int ans = inf;
+    for(int j=0;j<n;j++)
+    {
+        if(w[i][j] == inf)continue;
+
+        if(isTurnOn(j,mask)==0)
+        {
+            int res = f(j,turnOn(mask,j))+w[i][j];
+            ans = min(ans,res);
+        }
+    }
 }
 
 
 int main()
 {
-
+    memset();
+    memset();
 }
 
