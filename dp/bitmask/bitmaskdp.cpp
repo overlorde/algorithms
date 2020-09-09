c54acd7f5e2eff44b4fec1142e587edcbda16bee
saif <farhansaif488@gmail.com>
2020-09-09 19:13:11 +0600

    tested good

--- dp/bitmask/bitmaskdp.cpp
+++ dp/bitmask/bitmaskdp.cpp
@@ -12,7 +12,7 @@ int w[100][100];
 int mem[100][1000];
 int n=0;
 
-bool isTurnOn(int num,int pos)
+bool isOn(int num,int pos)
 {
     return (bool)(num & (1<<pos));
 }
@@ -21,29 +21,26 @@ int turnOn(int num,int pos)
     return (num | (1<<pos));
 }
 
-int f(int i,int mask)
-{
-    cout<<"entered "<<i<<" "<<mask<<endl;
-    if(mask == (1<<n)-1)
-    {
+int f(int i, int mask) {
+    if (mask == (1<<n) - 1) {
         return w[i][0];
     }
-    if(mem[i][mask] != -1)
-    {
+    
+    if (mem[i][mask] != -1) {
         return mem[i][mask];
     }
+    
     int ans = inf;
-    for(int j=0;j<n;j++)
-    {
-        if(w[i][j] == inf)continue;
-
-        if(isTurnOn(j,mask)==0)
-        {
-            int res = f(j,turnOn(mask,j))+w[i][j];
-            ans = min(ans,res);
+    for (int j = 0;j < n;j++) {
+        if (w[i][j] == inf) continue;
+        
+        if (isOn(mask,j) == 0) {
+            int result = f(j, turnOn(mask, j)) + w[i][j];
+            ans = min(ans, result);
         }
     }
-    return mem[i][mask]=ans;
+    
+    return mem[i][mask] = ans;
 }
 
 
