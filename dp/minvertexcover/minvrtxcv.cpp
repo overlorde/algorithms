3927e65bfe24f3a3cacd06fd482c28b7ef951b51
saif <farhansaif488@gmail.com>
2020-10-14 23:31:51 +0600

    --- Auto-Git Commit ---

--- dp/minvertexcover/minvrtxcv.cpp
+++ dp/minvertexcover/minvrtxcv.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define MAXX 100000
 
 int mem[MAXX][2];
-int par[MAXX];
+int par[MAXX];//parent
 vector<int> edges[MAXX];
 
 
@@ -19,6 +19,11 @@ int f(int i,int marker)
 
     for(auto x : edges[i])
     {
+        if(par[i]== x)
+        {
+            continue;
+        }
+
         if(marker == 0)
         {
             ans += f(x,1)
@@ -27,6 +32,7 @@ int f(int i,int marker)
             ans += 1+min(f(x,0),f(x,1));
         }
     }
+    return mem[i][marker]=ans;
 }
 
 
@@ -36,16 +42,15 @@ int main()
     memset(mem,-1,sizeof(par));
 
     int n;
-    cin>>n;
+    cin>>n;//number of bidirectional edges
 
-    for(int i=1;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
         int u,v;
         cin>>u>>v;
 
         edges[u].push_back(v);
         edges[v].push_back(u);
-
     }
 
     int ans = 0;
