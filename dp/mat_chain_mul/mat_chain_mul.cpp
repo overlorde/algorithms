cf7b8cf095ee69b5af632a3b2a0cc6b6b622ec4f
saif <farhansaif488@gmail.com>
2020-10-18 01:53:46 +0600

    --- Auto-Git Commit ---

--- dp/mat_chain_mul/mat_chain_mul.cpp
+++ dp/mat_chain_mul/mat_chain_mul.cpp
@@ -35,7 +35,20 @@ int f(int i,int j)
     {
         return mem[i][j];
     }
-    
+    int ans = INF;
+
+    for(int k = i;k<j;k++)
+    {
+        int res_left =  f(i,k);
+        int res_right = f(k+1,j);
+
+        int cost = (res_left+res_right)+mergeCost(i,j,k);
+        ans = min(ans,cost);
+    }
+
+    mem[i][j] = ans;
+    return mem[i][j];
+
 }
 
 
