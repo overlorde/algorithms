d566652832705758a2f43b08cf8ed1026b5266d8
saif <farhansaif488@gmail.com>
2020-10-18 03:43:29 +0600

    --- Auto-Git Commit ---

--- dp/mat_chain_mul/mat_chain_mul.cpp
+++ dp/mat_chain_mul/mat_chain_mul.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define MAX_N   100
 #define INF     1<<30
-
+#define EMPTY_VALUE -1
 
 
 int n=0,t=0;
@@ -26,11 +26,12 @@ vector<Matrix> mats;
 
 int mergeCost(int i,int j,int k)
 {
-    return mats[i].roW*mats[k].col*mats[j].col;
+    return (mats[i].row)*(mats[k].col)*(mats[j].col);
 }
 
 int f(int i,int j)
 {
+    //memset(mem,-1,sizeof(mem));
     if(i >= j)
     {
         return 0;
@@ -59,9 +60,10 @@ int f(int i,int j)
 int main()
 {
     cin>>t;
-
+    int h = t+1;
     while(t--)
     {
+        memset(mem,-1,sizeof(mem));
         mats.clear();
         cin>>n;
 
@@ -71,5 +73,8 @@ int main()
             cin>>r>>c;
             mats.push_back({r,c});
         }
+        int ans = f(1,n);
+        cout<<"Cout #"<<h-t<<ans<<endl;
+
     }   
 }
