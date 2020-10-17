f0c7be2ec3c93571f37e2b352f65d07ffe74a61d
saif <farhansaif488@gmail.com>
2020-10-18 01:36:56 +0600

    --- Auto-Git Commit ---

--- dp/mat_chain_mul/mat_chain_mul.cpp
+++ dp/mat_chain_mul/mat_chain_mul.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+#define MAX_N 100
+#define INF 1<<30
+
+int mem[MAX_N][MAX_N];
+
+struct Matrix{
+    int row,col;
+
+    
+    Matrix(int _row,int _col
+    {
+        row = _row;
+        col = _col;
+    }
+};
+
+vector<Matrix> mats;
+
+int mergeCost(int i,int j,int k)
+{
+    return mats[i].roW*mats[k].col*mats[j].col;
+}
+
+int f(int i,int j)
+{
+    if(i >= j)
+    {
+        return 0;
+    }
+    if(mem[i][j] != EMPTY_VALUE)
+    {
+        return mem[i][j];
+    }
+}
+
+
 int main()
 {
     
