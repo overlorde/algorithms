2c468ae30b72bac3e773ff3764fe8b6be7fbf4b3
saif <farhansaif488@gmail.com>
2020-10-18 06:23:33 +0600

    --- Auto-Git Commit ---

--- dp/test.cpp
+++ dp/test.cpp
@@ -1,42 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAX_N 20
-#define EMPTY_VALUE -1
 
-int mem[100][100];
+int mem[1000][1000];
 
-int lcsit(string S,string W)
-{
-    int n = S.size();
-    int m = W.size();
 
-    memset(mem,0,sizeof(mem));
-
-    for(int i=n-1;i>=0;i++)
-    {
-        for(int j =m-1;j>=0;j++)
-        {
-
-            if(S[i]==W[i])
-            {
-                mem[i][j]=mem[i+1][j+1]+1;
-
-            }
-            else{
-                mem[i][j]=max(mem[i+1][j],mem[i][j+1]);
-            }
-        }
-    }
-    return mem[0][0];
-}
 
 int main()
 {
-    string s,w;
-    cin>>s>>w;
+    int t;
+    cin>>t;
 
-    int ans = lcsit(s,w);
+    while(t--)
+    {
+        string s,w;
+        cin>>s>>w;
 
-    cout<<ans<<endl;
+        memset(mem,-1,sizeof(mem));
+
+        int ans = lcs(s,w);
+
+        cout<<ans<<endl;
+    }
 }
