08dfa7dfcae8db4b97ee915f75bc327b05c85925
saif <farhansaif488@gmail.com>
2020-11-05 04:30:46 +0600

    --- Auto-Git Commit ---

---
+++ uva_shanto/stl/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> mem;
+    mem.assign(50,1000);
+
+
+    for(int i=0;i<40;i++)
+    {
+        cout<<mem[i]<<" ";
+    }
+    return 0;
+}
