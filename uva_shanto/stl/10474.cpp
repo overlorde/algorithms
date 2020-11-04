11323a43cfd21266418cbd47186ca2afe31a5785
saif <farhansaif488@gmail.com>
2020-11-05 01:25:13 +0600

    --- Auto-Git Commit ---

--- uva_shanto/stl/10474.cpp
+++ uva_shanto/stl/10474.cpp
@@ -1,2 +1,40 @@
 
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll unsigned long long
+
+vector<ll> v[1001];
+
+vector<ll> mem;
+
+ll fibo(ll n)
+{
+    if(n==0 || n==1 || n==2)
+    {
+        return 1;
+    }
+    if(mem[n] != -1)
+    {
+        return mem[u];    
+    }
+
+    return mem[n] = fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    for(ll i=0;i<10001;i++)v[i].clear();
+    mem.clear();
+
+    ll x = fibo(900);
+
+    for(ll i=1;i<100;i++)
+    {
+        for(ll j=1;j<=900;j++)
+        {
+            v[i].push_back(mem[j]%j);
+        }
+    }
+}
