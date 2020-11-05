9b9339ba54bac7f60a69620e864c43959adadfc6
saif <farhansaif488@gmail.com>
2020-11-05 08:06:39 +0600

    --- Auto-Git Commit ---

--- usaco/sec1/gift1.cpp
+++ usaco/sec1/gift1.cpp
@@ -1,5 +1,66 @@
 /*
 ID: farhans4
 LANG: C++
-PROG: ride
+PROG: gift1
 */
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+typedef struct _person
+{
+	char name[100];
+	int money;
+}person;
+
+int main()
+{
+	int np;
+	int a,b,q,l,s;
+    FILE *in = fopen("gift1.in" ,"r");
+	FILE *out= fopen("gift1.out","w");
+
+	fscanf(in,"%d",&np);
+
+	person p[np];
+	char z[100];
+	int i;
+
+	for(i=0;i<np;i++)
+		p[i].money = 0;
+
+	for(i=0;i<np;i++)
+		fscanf(in,"%s",p[i].name);
+	
+	while(fscanf(in,"%s",z)!=EOF)
+	{
+		for(i=0;i<np;i++)
+		{
+			if(!strcmp(p[i].name,z))
+			{
+				fscanf(in,"%d %d",&a,&b);
+				if (b==0)
+					break;
+				p[i].money-=(a-a%b);
+				for (q=0;q<b;q++)
+				{
+					fscanf(in,"%s",z);
+					for(l=0;l<np;l++)
+					{
+						if(!strcmp(p[l].name,z))
+							p[l].money+=a/b;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	for (i=0; i<np; i++)
+		fprintf(out,"%s %d\n",p[i].name,p[i].money);
+
+	fclose(in);
+	fclose(out);
+	exit(0);
+}
--- uva_shanto/stl/test.cpp
+++ uva_shanto/stl/test.cpp
@@ -1,60 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*
+ID: your_id_here
+TASK: test
+LANG: C++                 
+*/
+/* LANG can be C++11 or C++14 for those more recent releases */
+#include <iostream>
+#include <fstream>
+#include <string>
 
-typedef struct _person
-{
-	char name[100];
-	int money;
-}person;
+using namespace std;
 
-void main()
-{
-	int np;
-	int a,b,q,l,s;
-    FILE *in = fopen("gift1.in" ,"r");
-	FILE *out= fopen("gift1.out","w");
-
-	fscanf(in,"%d",&np);
-
-	person p[np];
-	char z[100];
-	int i;
-
-	for(i=0;i<np;i++)
-		p[i].money = 0;
-
-	for(i=0;i<np;i++)
-		fscanf(in,"%s",p[i].name);
-	
-	while(fscanf(in,"%s",z)!=EOF)
-	{
-		for(i=0;i<np;i++)
-		{
-			if(!strcmp(p[i].name,z))
-			{
-				fscanf(in,"%d %d",&a,&b);
-				if (b==0)
-					break;
-				p[i].money-=(a-a%b);
-				for (q=0;q<b;q++)
-				{
-					fscanf(in,"%s",z);
-					for(l=0;l<np;l++)
-					{
-						if(!strcmp(p[l].name,z))
-							p[l].money+=a/b;
-					}
-				}
-				break;
-			}
-		}
-	}
-
-	for (i=0; i<np; i++)
-		fprintf(out,"%s %d\n",p[i].name,p[i].money);
-
-	fclose(in);
-	fclose(out);
-	exit(0);
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
 }
