0cd2da761b284f0709a6c4448567e888ecaf4a72
saif <farhansaif488@gmail.com>
2020-11-08 00:42:22 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/11582.cpp
+++ uva_shanto/math/11582.cpp
@@ -21,13 +21,10 @@ ll bigmod(ll a,ll b,ll M)
 
 ll fib(ll n)
 {
-    if(n == 0)
+    
+    if(n<=1)
     {
-        return 0;
-    }
-    if(n == 1 || n == 2)
-    {
-        return 1;
+        return n;
     }
 
     if(v[n]!=-1)
@@ -43,7 +40,7 @@ int main()
 {
 
     v.clear();
-    v.assign(101,-1);
+    v.assign(12,-1);
     //cout<<v[0];
     
     for(ll i=0;i<=100;i++)
@@ -53,9 +50,8 @@ int main()
     }
 
     ll x = fib(10);
-    v[0]=0;
+    
     v[1]=0;
-
     for(ll i=1;i<=10;i++)
     {
         cout<<v[i]<<" ";
