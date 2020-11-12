46b0661c7ebb77a4e31a85e779d7a0d3d9c07956
saif <farhansaif488@gmail.com>
2020-11-13 03:31:04 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/12169.cpp
+++ uva_shanto/math/12169.cpp
@@ -35,5 +35,34 @@ int main()
     for(a = 0;a<MOD;a++)
     {
         c = (x[3]-a*a*x[1]);
+        egcd(1LL*MOD,a+1,d,k,b);
+
+        if(c%d)continue;
+
+        b = b * c / d;
+
+        bool ok = true;
+
+        for(int i=2;i<=2*T;i++)
+        {
+            if(i&1)
+            {
+                if(x[i] != ((a*x[i-1]+b)%MOD)){
+                    ok = false;break;
+                }
+            }
+            else{
+                x[i] = ((a*x[i-1]+b)%MOD);
+            }
+        }
+        if(ok)
+        {
+            break;
+        }
+    }
+
+    for(int i=2;i<=2*T;i+=2)
+    {
+        cout<<x[i]<<endl;
     }
 }
