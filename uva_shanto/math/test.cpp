1ddcf2709433cb6602ca3ac9ecca76c6baa83b7c
saif <farhansaif488@gmail.com>
2020-11-12 18:23:35 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/test.cpp
+++ uva_shanto/math/test.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h><
 using namespace std;
 
-int egcd(int a,int b,int &x,int &y)
+#define ll long long
+
+ll egcd(ll a,ll b,ll &x,ll &y)
 {
   if(a==0)
   {
@@ -10,15 +12,15 @@ int egcd(int a,int b,int &x,int &y)
 
   }
 
-  int x1,y1;
-  int d = egcd(b%a,a,x1,y1);
+  ll x1,y1;
+  ll d = egcd(b%a,a,x1,y1);
 
   x = y1 - (b/a) * x1;
 
   y = x1;
 
   return d;
-  
+
 }
 
 
