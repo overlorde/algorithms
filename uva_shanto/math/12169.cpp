5b17ce392ee53fcad261355daaa88e926c10c989
saif <farhansaif488@gmail.com>
2020-11-12 19:05:39 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/12169.cpp
+++ uva_shanto/math/12169.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
+
+vector<ll> v;
+
+ll egcd(ll a,ll b,ll &x,ll &y)
+{
+    if(a==0)
+    {
+        x = 0;y = 0;
+        return b;
+    }
+    ll x1,y1;
+    ll d = egcd(b%a,a,x1,y1);
+
+    x = y1 - (b/a)*x1;
+
+    y = x1;
+
+    return d;
+}
+
 int main()
 {
-    
+    ll n;
+    cin>>n;
+
+    for(ll i=0;i<n;i++)
+    {
+        ll x;
+        cin>>x;
+        v.push_back(x);
+    }
 }
