234f2fe0371e54522686978c6bb0faf630bc8947
saif <farhansaif488@gmail.com>
2020-11-12 18:15:07 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/test.cpp
+++ uva_shanto/math/test.cpp
@@ -1,12 +1,28 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h><
 using namespace std;
+
+int egcd(int a,int b,int &x,int &y)
+{
+  if(a==0)
+  {
+    xd = 0;
+    y = 1;
+
+  }
+
+  int x1,y1;
+  int d = egcd(b%a,a,x1,y1);
+
+  x = y1 - (b/a) * x1;
+
+  y = x1;
+
+  return d;
+  
+}
+
 
 int main()
 {
-    vector<int> v;
-    v.clear();
 
-    v.push_back(1400);
-
-    cout<<v[0]<<endl;
 }
