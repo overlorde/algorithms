8f769c17ea244f10f5a4e1d82a735a5e3682b783
saif <farhansaif488@gmail.com>
2020-11-13 03:14:13 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/12169.cpp
+++ uva_shanto/math/12169.cpp
@@ -1,84 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+typedef long long ll;
+const int MAXD = 200+10, MOD = 10001;
+ll x[MAXD];
+lla ,k,b,d,c;
 
 
-vector<ll> v;
-
-ll egcd(ll a,ll b,ll &x,ll &y)
-{
-    if(a==0)
-    {
-        x = 0;y = 1;
-        return b;
-    }
-    ll x1,y1;
-    ll d = egcd(b%a,a,x1,y1);
-
-    x = y1 - ((b/a)*x1);
-
-    y = x1;
-
-    return d;
-}
-
-int main()
-{
-    ll n;
-    cin>>n;
-    v.clear();
-    v.assign(103,-1);
-    for(ll i=1;i<=2*n;i+=2)
-    {
-        ll x;
-        cin>>x;
-        v[i]=x;
-        //cout<<v[i]<<endl;
-    }
-    
-    
-    ll x3 = 0;
-    
-    for(ll a = 0;a<10001;a++)
-    {
-        ll x,y;
-        ll c = (v[3]-a*a*v[1]);
-        ll g = egcd(a+1,10001,x,y);
-        ll f =1;
-        if (c % g) continue;
-        x = (x*c)/g;
-
-        for(ll i=2;i<=2*n;i+=1)
-        {
-            
-            ll m = (((a*(v[i-1]))+x)%10001);
-            if(!i%2)
-            {
-                 v[i] = m;
-            }
-            else if(i%2){
-                
-                //cout<<x3<<endl;
-                if(m != v[i])
-                {
-                    
-                    f = 0;
-                    break;
-                }
-            }
-        }
-        if(f==1)
-       {
-           break;
-       }
-         
-    }
-
-    for(ll j = 2;j<=2*n;j+=2)
-    {
-        cout<<v[j]<<endl;
-    }
-
-    return 0;
-}
+ll x[MAXD];
