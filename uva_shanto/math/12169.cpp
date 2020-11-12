38bde321662eff77cf5ad065e0145d05f6560e5d
saif <farhansaif488@gmail.com>
2020-11-13 03:22:38 +0600

    --- Auto-Git Commit ---

--- uva_shanto/math/12169.cpp
+++ uva_shanto/math/12169.cpp
@@ -7,4 +7,33 @@ ll x[MAXD];
 lla ,k,b,d,c;
 
 
-ll x[MAXD];
+void egcd(ll a,ll b,ll &d,ll &x,ll &y)
+{
+    if(!b)
+    {
+        d = a;
+        x = 1;y = 0;
+        return;
+    }else{
+        egcd(b,a%b,d,y,x);
+        y -= x*(a/b);
+        return;
+    }
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+
+    for(int i=1;i<2*T;i+=)
+    {
+        cin>>x[i];
+    }
+
+
+    for(a = 0;a<MOD;a++)
+    {
+        c = (x[3]-a*a*x[1]);
+    }
+}
