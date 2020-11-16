e25a8f596d5c6c3b80413729a35f1b9a82ea5d20
saif <farhansaif488@gmail.com>
2020-11-17 00:50:12 +0600

    --- Auto-Git Commit ---

---
+++ uva_shanto/camp_graph/loj1026.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+vector<ll> adj[1001],d,vis,low,parent;
+vector<ll> articulation_point,count_ap;
+
+vector<pair<ll,ll> > bridges;
+
+ll t;
+
+void build_grpah()
+{
+    for(ll i=0;i<101;i++)
+    {
+        adj[i].clear();
+    }
+    ll n,m;
+
+    cout<<"Number of Nodes : ";
+    cin>>n;
+    cout<<"\nNumber of Brtidges : ";
+    cin>>m;
+    cout<<"\n";
+
+    for(ll i=0;i<m;i++)
+    {
+        ll u,v;
+        cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v]/.push_back(u);
+    }
+
+}
+
+void init()
+{
+    t = 0;
+    count_ap.clear();
+    bridges.clear();
+}
+
