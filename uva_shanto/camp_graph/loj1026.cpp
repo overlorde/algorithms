f8886014a4147557a6cf1fd601ae4890120a7cec
saif <farhansaif488@gmail.com>
2020-11-19 07:01:41 +0600

    --- Auto-Git Commit ---

--- uva_shanto/camp_graph/loj1026.cpp
+++ uva_shanto/camp_graph/loj1026.cpp
@@ -1,22 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 
-vector<ll> edges[101],d,vis,low,parent;
+vector<ll> adj[1001],d,vis,low,parent;
 vector<ll> articulation_point,count_ap;
 
 vector<pair<ll,ll>> bridges;
 
 ll t;
-ll s; // source may change each time i call find arti point functiobn
+
+void build_graph()
+{
+    for(ll i=0;i<101;i++)
+    {
+        adj[i].clear();
+    }
+    ll n,m;
+    cout<<"Number of Nodes : ";
+    cin>>n;
+    cout<<"'\n";
+    cout<<"Number of edges : ";
+    cin>>m;
+    cout<<"\n";
+
+    for(ll i=0;i<m;i++)
+    {
+        ll u,v;
+
+        cin>>u>>v;
+
+        adj[u].push_back(v);adj[v].push_back(u);
+    }
+
+}
 
 void init()
 {
     t = 0;
 
-    
-    
     count_ap.clear();
 
     bridges.clear();
@@ -25,17 +46,16 @@ void init()
     articulation_point.assign(101,-1);
 
     vis.clear();
-    vis.assign(101,0);
-
+    vis.assign(1001,0);
+    
     low.clear();
-    low.assign(101,0);
-
+    low.assign(1001,0);
+    
     d.clear();
-    d.assign(101,0);
+    d.assign(1001,0);
 
     parent.clear();
-    parent.assign(101,-1);
-    
+    parent.assign(1001,-1);
 }
 
 void FindArticulationPoint(ll u)
@@ -45,7 +65,7 @@ void FindArticulationPoint(ll u)
     vis[u]=1;
     ll no_of_children = 0;
 
-    for(auto v : edges[u])
+    for(auto v : adj[u])
     {
         if(v == parent[u]) continue;
 
@@ -60,7 +80,7 @@ void FindArticulationPoint(ll u)
             FindArticulationPoint(v);
             low[u] = min(low[u],low[v]);
 
-            if(d[u] <= low[v] && u != 1) //not root find //we are doing it manually,need to find reason
+            if(d[u] <= low[v] && u != 0) //not root find //we are doing it manually,need to find reason
             {
                 if(articulation_point[u] != 1)
                 count_ap.push_back(u);
@@ -69,7 +89,7 @@ void FindArticulationPoint(ll u)
             }
             no_of_children +=1;
         }
-        if(no_of_children > 1 && u == 1)
+        if(no_of_children > 1 && u == 0)
         {
             if(articulation_point[u] !=1)
             count_ap.push_back(u);
@@ -83,51 +103,24 @@ void FindArticulationPoint(ll u)
 
 int main()
 {
-    cin>>t;
-    int cnt = 0;
-    while(t--)
+    int cnt= 0;
+    while(1)
     {
         init();
-        for(ll i=0;i<=100;i++)
+        build_graph();
+        FindArticulationPoint(1);
+        cnt++;
+        cout<<"Test case : "<<cnt<<endl;
+        cout<<"Printing Articulation Points:\n";
+        for(auto n : count_ap)
         {
-            edges[i].clear();
+            cout<<n<<" ";
         }
-
-        ll n;
-        cin>>n;
-        //Input 
-        for(ll i=0;i<n;i++)
+        cout<<"\nPrinting Bridges : \n";
+        for(auto x : bridges)
         {
-            ll p;
-            cin>>p;
-
-            ll q;
-            cin>>q;
-
-            for(ll j=0;j<q;j++)
-            {
-                ll u;
-                cin>>u;
-                edges[p].push_back(u);
-            }
-
+            cout<<x.first<<" "<<x.second<<endl;            
         }
-        
-        for(ll k = 0;k<n;k++)
-        {
-            if(!vis[k])
-            {
-                s = k;
-                FindArticulationPoint(s);
-            }
-
-        }
-        cout<<"Test case:::"<<++cnt<<"\n";
-
-        for(auto y : bridges)
-        {
-            cout<<y.first<<" "<<y.second<<endl;
-        }
-
+        cout<<"\n";
     }
 }
