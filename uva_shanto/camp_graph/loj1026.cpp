d293c9b3513d08e8f811241630020d7e53e97aab
saif <farhansaif488@gmail.com>
2020-11-19 17:58:40 +0600

    --- Auto-Git Commit ---

--- uva_shanto/camp_graph/loj1026.cpp
+++ uva_shanto/camp_graph/loj1026.cpp
@@ -8,17 +8,17 @@ vector<ll> articulation_point,count_ap;
 vector<pair<ll,ll>> bridges;
 
 ll t;
-ll n,m;
+
 void build_graph()
 {
     for(ll i=0;i<101;i++)
     {
         adj[i].clear();
     }
-   // ll n,m;
+    ll n,m;
     cout<<"Number of Nodes : ";
     cin>>n;
-    cout<<"\n";
+    cout<<"'\n";
     cout<<"Number of edges : ";
     cin>>m;
     cout<<"\n";
@@ -80,26 +80,22 @@ void FindArticulationPoint(ll u)
             FindArticulationPoint(v);
             low[u] = min(low[u],low[v]);
 
-            if(d[u] <= low[v] && u != 0) //not root find //we are doing it manually,need to find reason
+            if(d[u] <= low[v] && u != 1) //not root find //we are doing it manually,need to find reason
             {
-                if(articulation_point[u] != 1){
+                if(articulation_point[u] != 1)
                 count_ap.push_back(u);
-                //cout<<"V : "<<v<<endl;
-                }
+            
                 articulation_point[u] = 1;
-               
             }
             no_of_children +=1;
         }
-        if(no_of_children > 1 && u == 0)
+        if(no_of_children > 1 && u == 1)
         {
-            if(articulation_point[u] !=1){
-            //cout<<"V (2nd if condition): "<<v<<endl;
+            if(articulation_point[u] !=1)
             count_ap.push_back(u);
-            }
+            
             articulation_point[u] = 1;
-            if(articulation_point[v]){
-            bridges.push_back({u,v});}
+            bridges.push_back({u,v});
 
         }
     }
@@ -126,10 +122,5 @@ int main()
             cout<<x.first<<" "<<x.second<<endl;            
         }
         cout<<"\n";
-        cout<<"Debug array: \n";
-        for(ll k = 1;k <= n;k++)
-        {
-            cout<<"NODE:"<<k<<"STATS:"<<articulation_point[k]<<endl;
-        }
     }
 }
