0d755c1a47336f3395079840d9084f5e206c5f11
saif <farhansaif488@gmail.com>
2020-11-22 02:34:40 +0600

    --- Auto-Git Commit ---

--- uva_shanto/camp_graph/bcc.cpp
+++ uva_shanto/camp_graph/bcc.cpp
@@ -10,3 +10,15 @@ vector<pair<ll,ll>> bridges;
 
 ll t;
 
+void build_graph()
+{
+    for(ll i=0;i<101;i++)
+    {
+        adj[i].clear();
+    }
+
+    ll n,m;
+    cout<<"Number of nodes:";
+    cin>>n;
+    cout
+}
