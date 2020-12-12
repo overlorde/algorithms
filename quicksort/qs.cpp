b1ba56d5d53d6b06ed93da8d6af1d26d1c1a603c
saif <farhansaif488@gmail.com>
2020-12-12 15:21:47 +0600

    --- Auto-Git Commit ---

--- quicksort/qs.cpp
+++ quicksort/qs.cpp
@@ -6,7 +6,26 @@ using namespace std;
 int partition(int arr[],int low , int high)
 {
     int pivot = arr[low];
-    int i = low - 1 , j = hight + 1;
+    int i = low - 1 , j = high + 1;
+    while(1)
+    {
+        while(arr[i]<pivot)
+        {
+            i++;
+
+        }
+    
+        while(arr[j]>pivot)
+        {
+            j--;
+        }
+        if(i>=j)
+        {
+            return j;
+        }
+        swap(arr[p],arr[j]);
+    }
+
     
 }
 
