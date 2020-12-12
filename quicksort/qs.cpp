9ce1cf3a24b6c53502f43d97e65187aeaf36283f
saif <farhansaif488@gmail.com>
2020-12-12 15:13:21 +0600

    --- Auto-Git Commit ---

---
+++ quicksort/qs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++>
+using namespace std;
+
+#define ll long long
+
+int partition(int arr[],int low , int high)
+{
+    int pivot = arr[low];
+    int i = low - 1 , j = hight + 1;
+    
+}
+
+int main()
+{
+
+}
