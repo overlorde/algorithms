f86af9ada47e017603dafa7db981b71c1de9a8ba
saif <farhansaif488@gmail.com>
2020-12-16 06:33:04 +0600

    --- Auto-Git Commit ---

--- quicksort/test.cpp
+++ quicksort/test.cpp
@@ -1,40 +1,46 @@
-#include <iostream>
-
-using namespace std;
-
-const int Max = 100;
-const int Min = 1;
-
-// No need to use std::sort since can just store counts in array
-int count[105];
-
-int main()
-{
-    int N;
-    while (cin >> N, N)
-    {
-        int age;
-        
-        for (int i = 0; i < N; ++i)
-        {
-            cin >> age;
-            ++count[age];
-        }
-        
-        string sep = "";
-        for (int i = Min; i <= Max; ++i)
-        {
-            // This will counteract how --count[i] works
-            // While leaving the count[i] == 0 after this iteration
-            ++count[i];
-            
-            while (--count[i])
-            {
-                cout << sep << i;
-                sep = " ";
-            }
-        }
-        
-        cout << '\n';
+/*
+ * Author: Quickgrid ( Asif Ahmed )
+ * Site: https://quickgrid.wordpress.com
+ * Problem: UVA 11462 ( Age sort )
+ * Solution Method: Quick Sort ( 2 Partition )
+ */
+#include<cstdio>
+#include<iostream>
+ 
+#define SIZE 2000002
+ 
+static int A[SIZE];
+ 
+void quicksort (int *a, register int n) {
+    register int i, j, mid;
+    if (n < 2) return;
+ 
+    mid = a[n >> 1];
+ 
+    for (i = 0, j = n - 1; ; i++, j--) {
+        while (a[i] < mid)
+            i++;
+        while (mid < a[j])
+            j--;
+ 
+        if (i >= j) break;
+        std::swap(a[i], a[j]);
     }
+    quicksort(a, i);
+    quicksort(a + i, n - i);
+}
+ 
+int main () {
+    register unsigned n, i;
+    while(scanf("%u", &n) && n){
+        for(i = 0; i < n; ++i)
+            scanf("%d", &A[i]);
+ 
+        quicksort(A, n);
+ 
+        for (i = 0; i < n; i++)
+            printf("%d%s", A[i], i == n - 1 ? "\n" : " ");
+ 
+    }
+    return 0;
 }
